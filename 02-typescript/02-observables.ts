40cfc239e77005d18f8d3370c0b7647c94682adb
davini10c <vinicio_520@hotmail.com>
2018-11-07 11:10:10 -0500

    ultimo

--- 02-typescript/02-observables.ts
+++ 02-typescript/02-observables.ts
@@ -6,15 +6,15 @@ const numeros$ = rxjs.of(1,2,3,4,5,6);
 
 console.log(numeros$);
 
-// numeros$
-//     .subscribe(
-//         (ok)=>{
-//             console.log('En ok',ok);
-//         },
-//         (error)=>{
-//             console.log('Error:',error);
-//         },
-//         ()=>{
-//             console.log('Complete');
-//         },
-//     );
+ numeros$
+     .subscribe(
+         (ok)=>{
+             console.log('En ok',ok);
+        },
+         (error)=>{
+            console.log('Error:',error);
+         },
+         ()=>{
+             console.log('Complete');
+         },
+     );
