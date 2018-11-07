f03c60b17c17fe1a076ac1086656392676c45fa8
davini10c <vinicio_520@hotmail.com>
2018-11-07 10:52:37 -0500

    ultimo

--- 02-typescript/02-observables.ts
+++ 02-typescript/02-observables.ts
@@ -1,15 +1,20 @@
-//02-observables.ts
-
-declare var require: any;
-
+// 02-observables.ts
+declare var require:any;
 const rxjs = require('rxjs');
 
-const observable$ = rxjs.of(1,2,3,4,5,6);
+const numeros$ = rxjs.of(1,2,3,4,5,6);
 
-numeros$
-    .subscribe(
-    
+console.log(numeros$);
 
-
-
-    );
+// numeros$
+//     .subscribe(
+//         (ok)=>{
+//             console.log('En ok',ok);
+//         },
+//         (error)=>{
+//             console.log('Error:',error);
+//         },
+//         ()=>{
+//             console.log('Complete');
+//         },
+//     );
