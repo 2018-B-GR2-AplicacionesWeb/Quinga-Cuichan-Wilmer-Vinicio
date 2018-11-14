09c0d6ace757f6f1877b6ba9f8bfd12f47ae04aa
davini10c <vinicio_520@hotmail.com>
2018-11-13 21:17:01 -0500

    observables

--- 02-typescript/02-observables.js
+++ 02-typescript/02-observables.js
@@ -1,62 +1,91 @@
-const fs = require('fs');
+//02-observables.ts
+//declare var Promise: any;
 const rxjs = require('rxjs');
 const map = require('rxjs/operators').map;
 const distinct = require('rxjs/operators').distinct;
 const concat = require('rxjs/operators').concat;
-const numeros$ = rxjs.of(1, true, 1, { nombre: 'vinicio' }, ['oli'], function () {
+//con el signo de dolar es observable
+const numeros$ = rxjs.of(2, 2, 'Vinicio', { nombre: 'Vinicio' }, ['oli'], function () {
 });
-console.log(numeros$);
+//subcribe la mas importante
+//3 funciones
+//pipe
+/*
 numeros$
-    .pipe(distinct(), map((valorActual) => {
-    return {
-        data: valorActual
-    };
-}))
-    .subscribe((ok) => {
-    console.log('En ok', ok);
-}, (error) => {
-    console.log('Error:', error);
-}, () => {
-    console.log('Complete');
-});
+
+//cualquier operacion dentro del pipe
+    .pipe(
+        distinct(),//elimina los repetidos
+
+        //funcion anonima
+        map(
+            (valorActual) => {
+                //  return false;//se crea un arreglo de falsos
+
+                return {
+                    data: valorActual
+                    //permite despleglar todos los valores del arreglo
+                };
+
+            }
+        )
+    )
+    .subscribe(
+        //cuandotodo este bn
+        (ok) => {
+            //esta funcion se ejecuta cuando complilas es codigo
+            console.log('En ok', ok)
+
+        },
+        (error) => {
+            console.log('En error', error)
+
+        },
+        //cuando de complete algo
+        () => {
+
+        }
+    );
+*/
 const promesita = (funciona) => {
-    return new Promise((resolver, reject) => {
+    return new Promise((resolve, reject) => {
         if (funciona) {
-            resolver(' :) ');
+            resolve(' :) ');
         }
         else {
             reject(' :( ');
         }
     });
 };
-const promesita$ = rxjs.from(promesita(false));
+const promesita$ = rxjs.from(promesita(true)); //le transforma la promesa
+//en observables
 promesita$
     .subscribe((ok) => {
-    console.log('Promesita bien', ok);
+    console.log('Promesita bien ', ok);
 }, (error) => {
     console.log('Promesita mal', error);
 }, () => {
-    console.log('completado');
+    console.log('Completado');
 });
+//funcion para concatenar observables
+// es un nuevo observable con los 2 datos de los onservables
 const observableConcatenado$ = numeros$
-    .pipe(concat(promesita$)
-/*  map(
-
-      ()=>{
-
-          console.log('Ejecuto');
-          return
-          data: valorActual
-
-      }
-
-  )*/
-);
+    .pipe(//todas las funciones dentro del pipe
+concat(promesita$), //permite concatenar observables
+distinct(), //elimina repetidos
+map(//funcion anonima
+(valorActual) => {
+    console.log('Ejecuto'); //
+    return {
+        data: valorActual //devuelve el valor de la promesa uno por unos
+    };
+}));
+//despliega los datos concatenados
 observableConcatenado$
     .subscribe((ok) => {
-    console.log('Promesita bien', ok);
+    console.log('Concatenado bien ', ok);
 }, (error) => {
-    console.log('Promesita mal', error);
+    console.log('Error', error);
 }, () => {
-    console.log('completado');
+    console.log('Completado');
 });
--- 02-typescript/02-observables.ts
+++ 02-typescript/02-observables.ts
@@ -1,8 +1,7 @@
-// 02-observables.ts
+//02-observables.ts
+
 declare var require: any;
 declare var Promise: any;
-
-const fs = require('fs');
 
 
 const rxjs = require('rxjs');
@@ -11,109 +10,107 @@ const distinct = require('rxjs/operators').distinct;
 const concat = require('rxjs/operators').concat;
 
 
-const numeros$ = rxjs.of(1, true, 1, {nombre: 'vinicio'}, ['oli'], function () {
+//con el signo de dolar es observable
+const numeros$ = rxjs.of(2, 2, 'Vinicio', {nombre: 'Vinicio'}, ['oli'], function () {
 
 });
 
-console.log(numeros$);
+//subcribe la mas importante
+//3 funciones
 
+//pipe
+/*
 numeros$
+
+//cualquier operacion dentro del pipe
     .pipe(
-        distinct(),
+        distinct(),//elimina los repetidos
+
+        //funcion anonima
         map(
             (valorActual) => {
+                //  return false;//se crea un arreglo de falsos
+
                 return {
                     data: valorActual
-
-
+                    //permite despleglar todos los valores del arreglo
                 };
 
             }
         )
     )
-
     .subscribe(
+        //cuandotodo este bn
         (ok) => {
-            console.log('En ok', ok);
+            //esta funcion se ejecuta cuando complilas es codigo
+            console.log('En ok', ok)
+
         },
         (error) => {
-            console.log('Error:', error);
+            console.log('En error', error)
+
         },
+        //cuando de complete algo
         () => {
-            console.log('Complete');
-        },
-    );
-
-
-const promesita = (funciona: boolean): Promise<string> => {
-
-    return new Promise(
-        (resolver, reject) => {
-            if (funciona) {
-                resolver(' :) ');
-
-            }
-            else {
-                reject(' :( ');
-
-            }
-
 
         }
-    )
+    );
+*/
+
+const promesita = (funciona: boolean): Promise<string> => {
+    return new Promise(
+        (resolve, reject) => {
+            if (funciona) {
+                resolve(' :) ');
+            } else {
+                reject(' :( ');
+            }
+        }
+    );
 };
 
-const promesita$= rxjs.from(promesita(false));
+const promesita$ = rxjs.from(promesita(true));//le transforma la promesa
+//en observables
 promesita$
     .subscribe(
         (ok) => {
-            console.log('Promesita bien', ok)
+            console.log('Promesita bien ', ok);
         },
         (error) => {
-
-            console.log('Promesita mal', error)
-
+            console.log('Promesita mal', error);
         },
         () => {
-            console.log('completado')
-
-
+            console.log('Completado');
         }
     );
 
-
+//funcion para concatenar observables
+// es un nuevo observable con los 2 datos de los onservables
 const observableConcatenado$ = numeros$
-    .pipe(
-      concat(promesita$)
-      /*  map(
-
-            ()=>{
-
-                console.log('Ejecuto');
-                return
-                data: valorActual
-
+    .pipe(//todas las funciones dentro del pipe
+        concat(promesita$),//permite concatenar observables
+        distinct(),//elimina repetidos
+        map(//funcion anonima
+            (valorActual) => {
+                console.log('Ejecuto');//
+                return {
+                    data: valorActual//devuelve el valor de la promesa uno por unos
+                };
             }
+        )
+    );
 
-        )*/
-    )
-
+//despliega los datos concatenados
 
 observableConcatenado$
     .subscribe(
-
         (ok) => {
-            console.log('Promesita bien', ok)
+            console.log('Concatenado bien ', ok);
         },
         (error) => {
-
-            console.log('Promesita mal', error)
-
+            console.log('Error', error);
         },
         () => {
-            console.log('completado')
-
-
+            console.log('Completado');
         }
-
     );
